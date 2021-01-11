6416f7d9911f34958aa37e2c63696b15227213ba
Siddharth Shrivastava <44302419+Siddharth2430@users.noreply.github.com>
2021-01-11 21:37:15 +0530

    Added First app - myapp

---
+++ myapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! ");
+			Console.WriteLine(args[0]);
+			Console.WriteLine(args[1]);
+        }
+    }
+}
